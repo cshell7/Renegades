298b023356ed66a685a6f847f47fb9057422a3cc
Calvin Shell <shell.calvin7@gmail.com>
2015-12-08 20:38:58 -0700

    wip

---
+++ _site/js/renegades.js
@@ -0,0 +1,33 @@
+
+//Smooth Scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function navVideo() {
+  console.log('navtest')
+  location.href = "#video";
+}
+
+function onYouTubePlayerReady(playerId) {
+    var ytplayer = document.getElementById('ytplayer');
+    ytplayer.playVideo();
+}
